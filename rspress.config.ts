72df40c0ddd32daea925bd06bdcfcadc421997b3
Lam <daibanglam@gmail.com>
2025-05-09 13:59:56 +0700

    set custom font for better reading

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'rspress/config';
 import katex from 'rspress-plugin-katex';
+import path from 'path';
 
 export default defineConfig({
   root: 'docs',
@@ -10,5 +11,10 @@ export default defineConfig({
     lastUpdated: true,
     enableScrollToTop: true,
   },
+
+  // enable math syntax
   plugins: [katex()],
+
+  // add custom styles
+  globalStyles: path.join(__dirname, 'assets/styles.css'),
 });
