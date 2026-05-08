f1c2630f8b54c1fba8559d19847e85438933622e
Lam <daibanglam@gmail.com>
2026-05-08 16:42:59 +0700

    simplify rspress config and dependencies

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,33 +1,17 @@
 import * as path from 'node:path';
-import { defineConfig, UserConfig } from 'rspress/config';
+import { defineConfig } from 'rspress/config';
 import katex from 'rspress-plugin-katex';
 import mermaid from 'rspress-plugin-mermaid';
 
-const projectRoot = __dirname;
-const docRoot = path.join(projectRoot, 'docs');
-const baseConfig: UserConfig = {
+export default defineConfig({
   title: "Albert's Cheat sheet",
-  root: docRoot,
+  root: path.join(__dirname, 'docs'),
   icon: '/logo.svg',
   logo: '/logo.svg',
   themeConfig: {
     lastUpdated: true,
     enableScrollToTop: true,
   },
-
   // enable math syntax
   plugins: [katex(), mermaid()],
-
-  // add custom styles
-  globalStyles: path.join(projectRoot, 'assets', 'rspress-styles.css'),
-};
-
-// Used by Cheat Sheet Utils extension to create custom config for previewing
-export function createConfig(config?: UserConfig) {
-  return defineConfig({
-    ...baseConfig,
-    ...config,
-  });
-}
-
-export default defineConfig(baseConfig);
+});
